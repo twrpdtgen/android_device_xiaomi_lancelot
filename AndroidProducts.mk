928a39ee5cb11ba5989d38d59bf69e20c4f4b848
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-01 02:45:12 +0200

    lancelot: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: lancelot Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_lancelot.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_lancelot-user \
+    omni_lancelot-userdebug \
+    omni_lancelot-eng
